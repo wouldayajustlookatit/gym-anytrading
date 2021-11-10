159d5c98c8c7a0eba24e68db812152094f9f1883
wouldayajustlookatit <44936661+wouldayajustlookatit@users.noreply.github.com>
2021-11-09 19:46:59 -0500

    init

--- gym_anytrading/envs/stocks_env.py
+++ gym_anytrading/envs/stocks_env.py
@@ -16,7 +16,7 @@ class StocksEnv(TradingEnv):
 
 
     def _process_data(self):
-        prices = self.df.loc[:, 'Close'].to_numpy()
+        prices = self.df.loc[:, 'close'].to_numpy()
 
         prices[self.frame_bound[0] - self.window_size]  # validate index (TODO: Improve validation)
         prices = prices[self.frame_bound[0]-self.window_size:self.frame_bound[1]]
